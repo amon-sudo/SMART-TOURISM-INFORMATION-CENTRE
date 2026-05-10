47de4f0d3bc6996c8f1c11143e419af84850cb6a
bryan-kipla <bryankiplagat549@gmail.com>
2026-05-10 21:25:08 +0300

    Set up environment and init security configs

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from app import create_app
+import os
 from dotenv import load_dotenv
+from flask_migrate import Migrate
+from app import create_app, db   # import db from extensions
+
 load_dotenv()
+
 
 app = create_app()
 
-if  __name__== "__main__":
+
+migrate = Migrate(app, db)
+
+if __name__ == "__main__":
     app.run(debug=True)
