2f8496f74eaac10c31c8485de737f35441f52c56
Amon <amonogino@gmail.com>
2026-05-14 12:46:58 +0300

    whatever it is

--- app/Business/Business_Profile/MVC_architecture/Business_profile_models/Business_profile_domain/Business_profile_domain.py
+++ app/Business/Business_Profile/MVC_architecture/Business_profile_models/Business_profile_domain/Business_profile_domain.py
@@ -14,7 +14,7 @@ class BusinessProfile(db.Model):
     registration_request_id = db.Column(db.Uuid(as_uuid=True), db.ForeignKey("business_registration_requests.id"), nullable=True, unique=True)
     business_type = db.Column(db.String(50), nullable=True)
     business_name = db.Column(db.String(255), nullable=True)
-    media_url = db.Column(db.UUID(as_uuid=True), db.ForeignKey("media.id", ondelete="SET NULL"), nullable=True)
+    # media_url = db.Column(db.UUID(as_uuid=True), db.ForeignKey("media.id", ondelete="SET NULL"), nullable=True)
     verified = db.Column(db.Boolean, nullable=False, default=False)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
     created_at = db.Column(
